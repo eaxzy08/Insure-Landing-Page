d3ff892ac8bcaae87e54800f57aef4236fe46fa4
eaxzy08 <ogundejiissa08@gmail.com>
2023-11-06 14:34:18 +0100

    first commit

--- Corrections/script.js
+++
@@ -1,6 +0,0 @@
-let navMenu = document.getElementById("nav-menu");
-let menuIcon = document.getElementById("menu-icon");
-
-menuIcon.addEventListener('click', () => {
-    navMenu.classList.toggle('active')
-})
